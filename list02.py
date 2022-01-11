d6e85c462b45ceb93619c1ca38628de37db9b98f
Muu-X <46626240+Muu-X@users.noreply.github.com>
2022-01-11 22:03:02 +0800

    Update list02.py

--- list02.py
+++ list02.py
@@ -1,12 +1,11 @@
-# fn = input("Enter a file name:") fh = open(fn)
-
 fh = open("mbox-short.txt")
+
 count = 0
 for line in fh:
     line = line.rstrip()
-    if line.startswith("From"):
-        word = line.split()
+    word = line.split()
+    if len(word) >= 2 and line.startswith("From"):
         print(word[1])
         count = count+1
-print("so far so good")
-print("there were",count, "lines in the file with From")
+
+print("there were",count, "lines in the file with From as the first word")
